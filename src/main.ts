60e030feb4cbbf92ce10af016fd90341869896a2
Pedro Leonardo <pedrofire7180@gmail.com>
2026-05-14 19:54:10 -0300

    feat: enable cors and change dockerfile to pg16

--- src/main.ts
+++ src/main.ts
@@ -32,6 +32,11 @@ async function bootstrap() {
     }),
   );
 
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 
